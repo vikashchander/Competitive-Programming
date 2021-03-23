892e6332125255d0ee2411888848d7e64eeb9838
vikashchander <vikashchander78384@gmail.com>
2021-03-23 18:16:47 +0530

    cookoff: weightBal && create gitIgnore file

---
+++ march-codechef/weightBal.java
@@ -0,0 +1,74 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class weightBal {
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code goes here
+        FastReader scan = new FastReader();
+        int t = scan.nextInt();
+        while (t-- > 0) {
+            int n = scan.nextInt();
+            int m = scan.nextInt();
+            int x1 = scan.nextInt();
+            int x2 = scan.nextInt();
+            int mon = scan.nextInt();
+
+            int min = n + (mon * x1);
+            int max = n + (mon * x2);
+
+            if (min <= m && max >= m) {
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+            }
+
+        }
+    }
+}

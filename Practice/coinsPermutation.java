674f29aa15a650344be8248a48ac9b5157cd8b7f
vikashchander <vikashchander78384@gmail.com>
2021-03-25 01:57:32 +0530

    dp: coins permutations

---
+++ Practice/coinsPermutation.java
@@ -0,0 +1,82 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class coinsCombination {
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code goes here
+        FastReader scan = new FastReader();
+        // int t = scan.nextInt();
+
+        int num = scan.nextInt();
+        int amt = scan.nextInt();
+        int[] coins = new int[num];
+        for (int i = 0; i < num; i++) {
+            coins[i] = scan.nextInt();
+        }
+        problem(num, amt, coins);
+    }
+
+    public static void problem(int num, int amt, int[] coins) {
+        int[] dp = new int[amt + 1];
+        int mod = 1000000007;
+        dp[0] = 1;
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 0; j < coins.length; j++) {
+
+                if (i - coins[j] >= 0) {
+                    dp[i] += dp[i - coins[j]];
+                    dp[i] %= mod;
+                }
+
+            }
+        }
+        System.out.println(dp[amt]);
+    }
+}

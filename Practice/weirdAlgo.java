9a22a9d87aeebd20c47f7abe2306795ab8579a17
vikashchander <vikashchander78384@gmail.com>
2021-03-25 02:50:06 +0530

    Intro Prob: weirdAlgo

---
+++ Practice/weirdAlgo.java
@@ -0,0 +1,77 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class weirdAlgo {
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code goes here
+        FastReader scan = new FastReader();
+        // int t = scan.nextInt();
+
+        int num = scan.nextInt();
+
+        problem(num);
+
+    }
+
+    public static void problem(int num) {
+        System.out.print(num + " ");
+        while (num != 1) {
+
+            if (num % 2 == 0) {
+                num = num / 2;
+            } else {
+                num = (num * 3) + 1;
+            }
+            System.out.print(num + " ");
+
+        }
+
+    }
+}

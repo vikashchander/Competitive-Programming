16de8b7135c837ddd5302789bafacb3304d937d5
vikashchander <vikashchander78384@gmail.com>
2021-04-22 00:26:26 +0530

    codeforces problem

---
+++ codeforces/tit.java
@@ -0,0 +1,96 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+public class tit {
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code goes here
+        FastReader scan = new FastReader();
+        int t = scan.nextInt();
+        while (t-- > 0) {
+            int n = scan.nextInt();
+            int k = scan.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = scan.nextInt();
+            }
+
+            for (int i = 0; i < n - 1; i++) {
+                if (arr[i] < k) {
+                    k -= arr[i];
+                    arr[n - 1] += arr[i];
+                    arr[i] = 0;
+                } else {
+                    arr[i] -= k;
+                    arr[n - 1] += k;
+                    k = 0;
+                }
+            }
+
+            for (int i : arr) {
+                System.out.print(i + " ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void problem(int[] arr, int n) {
+        int sum = 0;
+        // System.out.println(sum);
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 1 || arr[i] == 3) {
+                // System.out.println(arr[i] + " -> " + " ada ");
+                sum = sum + 1;
+            }
+        }
+        System.out.println(sum);
+
+    }
+}

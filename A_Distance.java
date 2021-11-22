fdb58daeb58a2a1fe640c07e48bf5bb4b0767303
Vikash Chander <28572584+vikashchander@users.noreply.github.com>
2021-11-22 19:52:32 +0530

    codeforces : A_Distance

---
+++ A_Distance.java
@@ -0,0 +1,71 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+public class A_Distance{
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code goes here
+        FastReader scn = new FastReader();
+        int test = scn.nextInt();
+        while(test-->0){
+            int x = scn.nextInt();
+            int y = scn.nextInt();
+            int val = x+y;
+
+            if(val%2 != 0){
+                System.out.println(-1+" "+-1);
+            }else{
+                // System.out.println(y/2+y%2+" ->>");
+                System.out.println(x/2+" "+(y/2+y%2));
+            }
+        }
+        
+    }
+
+}

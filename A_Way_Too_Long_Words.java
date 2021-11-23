005b363d92089f1b9aee5f8b4eefd7dfc5c48923
Vikash Chander <28572584+vikashchander@users.noreply.github.com>
2021-11-23 23:06:35 +0530

    codeforces: way too long words

---
+++ A_Way_Too_Long_Words.java
@@ -0,0 +1,75 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+public class A_Way_Too_Long_Words{
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code goes here
+        FastReader scn = new FastReader();
+         int n = scn.nextInt();
+
+         while(n-->0){
+             String str = scn.nextLine();
+             int len = str.length();
+            
+             if(len>10){
+               StringBuilder sb = new StringBuilder();
+               sb.append(str.charAt(0));
+               sb.append(len-2);
+               sb.append(str.charAt(len-1));
+
+               System.out.println(sb.toString());
+             }else{
+                 System.out.println(str);
+             }
+         }
+       
+    }
+
+}
